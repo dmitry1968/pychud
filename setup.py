e8cf7bd4c6c0703a34f49c6e86055815c91d9f24
Dmitry Mikhin <dmitry.mikhin@gmail.com>
2020-12-20 13:51:57 +1100

    Rewrite setup.py from scratch. Easier than sorting out problems in the existing code. Compiles and installs (at least when tested under conda), tests pass.

--- setup.py
+++ setup.py
@@ -1,56 +1,36 @@
-NAME = 'pychud'
-VERSION = '0.1.dev1'
-AUTHOR = 'Matthew W. Hoffman'
-AUTHOR_EMAIL = 'mwh30@cam.ac.uk'
+from numpy.distutils.core import Extension, setup
+from numpy.distutils.system_info import get_info, NotFoundError
 
 
-def setup_package(parent_package='', top_path=None):
-    from numpy.distutils.core import setup
-    from numpy.distutils.system_info import get_info, NotFoundError
-    from numpy.distutils.misc_util import Configuration
+def setup_package(src_files, **kwargs):
+    """ 
+    Get source files and create a list of options for compilation.
+    """
+    fext = Extension(name="pychud",
+                     sources=src_files,
+                     extra_compile_args=["-O3"],
+                     **kwargs)
 
-    sources = ['src/pychud.pyf', 'src/dchud.f', 'src/dchdd.f']
-    extra_info = {}
-    extra_link_args = []
-
-    try:
-        # try to build against the MKL.
-        extra_info = get_info('mkl', 2)
-        extra_link_args = ['-Wl,-rpath,'+path for path in extra_info['library_dirs']],
-        print 'Linking against the MKL'
-
-    except NotFoundError:
-        try:
-            # With no MKL found we'll try to link against whatever version of
-            # BLAS is installed system-wide.
-            extra_info = get_info('blas', 2)
-            extra_link_args = ['-Wl,-rpath,'+path for path in extra_info['library_dirs']],
-            print 'Linking against BLAS'
-
-        except NotFoundError:
-            # Found nothing. So we'll build the necessary linear algebra
-            # functions ourselves.
-            sources += ['src/ddot.f', 'src/dnrm2.f', 'src/drotg.f']
-            print 'No BLAS found. Building BLAS functions manually'
-
-    config = Configuration()
-    config.add_extension(
-        name='pychud',
-        sources=sources,
-        extra_info=extra_info,
-        extra_link_args=extra_link_args
-        )
-
-    # run the setup.
-    setup(
-        name=NAME,
-        version=VERSION,
-        author=AUTHOR,
-        author_email=AUTHOR_EMAIL,
-        **config.todict()
-        )
-
+    setup(name='pychud',
+          version='0.1',
+          description="Attempt to provide cholupdate in Python",
+          author="Dmitry Mikhin",
+          author_email="dmitry.mikhin@gmail.com",
+          tests_require=['pytest'],
+          ext_modules=[fext])
 
 if __name__ == '__main__':
-    setup_package()
+    sources = ['src/pychud.pyf', 'src/dchud.f', 'src/dchdd.f']
 
+    config = {}
+    try:
+        extra_info = get_info('mkl', 2)
+        config['extra_link_args'] = [f'-l{lib}' for lib in extra_info['libraries']]
+    except NotFoundError:
+        try:
+            extra_info = get_info('blas', 2)
+            config['extra_link_args'] = [f'-l{lib}' for lib in extra_info['libraries']]
+        except NotFoundError:
+            sources.extend(['src/ddot.f', 'src/dnrm2.f', 'src/drotg.f'])
+
+    setup_package(sources, **config)
